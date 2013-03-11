21197ec71feb990f7fbedc69043329f75186d549
Ivan Willig <iwillig@gmail.com>
2013-03-11 12:53:28 -0400

    Instead of using the setMaxSize function on the window resize method, use the parse url

--- media/script/hashUrl.js
+++ media/script/hashUrl.js
@@ -1,12 +1,27 @@
 /*global $, window, app, Ext */
 (function (window, Ext) {
     'use strict';
-    var doHashUrl, setMaxSize;
+    var doHashUrl, setMaxSize, setOriginalSize, setButtonState;
+
+    setButtonState = function (button) {
+
+    };
+
+// in the playback tool bar we store the original size as a property
+// on the portal
+    setOriginalSize = function (app) {
+        if (typeof app.originalSize === 'undefined') {
+            app.portal.originalSize = app.portal.getSize();
+        }
+        return app;
+    };
 
     setMaxSize = function (app) {
 // calculate the height of the header in order to exclude that value
 // from the fullscreen size
-        var headerHeight = Ext.get('header').getHeight() +
+
+        var headerHeight =
+            Ext.get('header').getHeight() +
             Ext.get('top-crossbar').getHeight() +
             Ext.get('crossbar').getHeight(),
 
@@ -14,9 +29,13 @@
             newWidth = window.innerWidth + 1, // why the +1 ?
             newHeight = window.innerHeight - headerHeight + 2; // why ?
 
+
+        setOriginalSize(app);
+
         app.portal.setSize(newWidth, newHeight);
         app.portal.el.alignTo(main, 'tl-tl', [-8, 0]);
         Ext.getBody().setStyle({overflow: 'hidden'});
+        window.scrollTo(0, 0);
 // TODO at this point we need to switch the state of the button and
 // maybe set the originalSize property of the app.portal object
     };
@@ -29,12 +48,13 @@
 
             if (hash === '#full') {
                 setMaxSize(app);
+            } else {
+                //setOriginalSize(app);
             }
         };
 
 // wait until the portal has everything it needs to run
         app.on('portalready', function () {
-            debugger;
             parseUrl();
         });
 
@@ -42,9 +62,10 @@
 // is in full screen mode
 
         Ext.EventManager.onWindowResize(function () {
-            setMaxSize(app);
+            parseUrl();
         });
-        // make sure we have the onhashchange event
+
+// make sure we have the onhashchange event
         if (typeof window.onhashchange !== 'undefined') {
             window.onhashchange = function (event) {
                 parseUrl();
