541fa833a0b1abb80f036e773bd095b059204f3c
Ivan Willig <iwillig@gmail.com>
2013-04-29 11:52:11 -0400

    Remove underscore as a dep

--- media/script/map-search.js
+++ media/script/map-search.js
@@ -3,13 +3,64 @@
 
 (function ($) {
     'use strict';
-    var LayerResult, LayerSearch;
+    var LayerResult, LayerSearch, layerElementTemplate, widgetTemplate;
+
+
+    layerElementTemplate = new Ext.Template(
+        '<div class="ms-layer-title">',
+        '<p>',
+        '<a class="show-meta" href="#">{title}</a> by ',
+        '<a href="<%= layer.owner_detail %>">{owner}</a> on ',
+        '{last_modified}',
+        '<a class="ms-add-to-map" href="#">Add to map</a>',
+        '</p>',
+        '</div>',
+        '<div class="ms-layer-info">',
+        '<img src="{thumb}">',
+        '<p class="ms-layer-rating">{views} Views |',
+        ' {rating} Rating</p>',
+        '<div class="ms-layer-abstract">{abstract}</div>',
+        '</div>'
+
+    ).compile();
+
+    // maybe this template should live in the html document
+    widgetTemplate = new Ext.Template(
+    '<div id="ms-header">',
+      '<form>',
+        '<fieldset>',
+          '<button id="search" type="submit" class="btn">Search</button>',
+          '<input id="query" type="text" class="search-query">',
+          '<select id="sortBy">',
+            '<option value="newest">Newest</option>',
+            '<option value="oldest">Oldest</option>',
+            '<option value="alphaaz">Alphabetical (A-Z)</option>',
+            '<option value="alphaza">Alphabetical (Z-A)</option>',
+            '<option value="popularity">Popularity</option>',
+            '<option value="rel">Relevance</option>',
+          '</select>',
+        '</fieldset>',
+        '<fieldset>',
+          '<label>Limit layers current map area</label>',
+          '<input id="bbox-limit" type="checkbox">',
+          '<label>Show meta info expanded</label>',
+          '<input id="show-meta-info" type="checkbox" checked>',
+        '</fieldset>',
+      '</form>',
+    '</div>',
+    '<div id="ms-search-layers">',
+      '<ul>',
+      '</ul>',
+    '</div>',
+    '<div id="ms-footer">',
+      '<button id="done" class="btn">Done</button>',
+    '</div>').compile();
 
     LayerResult = function (options) {
         this.$el = $('<li>');
         this.layer = options.layer;
         this.geoExplorer = options.geoExplorer;
-        this.template = _.template($('#layer-element').html());
+        this.template = layerElementTemplate;
     };
 
     LayerResult.prototype.checkLayerSource = function (callback) {
@@ -63,9 +114,9 @@
     };
 
     LayerResult.prototype.render = function (showMeta) {
-        this.$el.html(this.template({
-            layer: this.layer
-        }));
+
+        this.$el.html(this.template.apply(this.layer));
+
 
         if (!showMeta) {
             this.$el.find('div.ms-layer-info').hide();
@@ -76,8 +127,15 @@
             slicePoint: 200
         });
 
-        this.$el.find('a.ms-add-to-map').click(_.bind(this.addToMap, this));
-        this.$el.find('.show-meta').click(_.bind(this.toggleInfo, this));
+
+        this.$el.find('a.ms-add-to-map').click(
+            Ext.createDelegate(this.addToMap, this)
+        );
+
+        this.$el.find('.show-meta').click(
+            Ext.createDelegate(this.toggleInfo, this)
+        );
+
         return this;
     };
 
@@ -91,11 +149,12 @@
             id: 'ms-search-widget'
         });
 
-        $(window).resize(_.bind(this.setLeft, this));
+        $(window).resize(
+            Ext.createDelegate(this.setLeft, this)
+        );
 
         this.setLeft();
-
-        this.template = _.template($('#add-layer-template').html());
+        this.template = widgetTemplate;
 
     };
 
@@ -117,6 +176,8 @@
         var self = this,
             showMeta = this.$el.find('#show-meta-info:checkbox').is(':checked'),
             queryParameters = {
+                // hard code the type as it does not sense to add a
+                // map to another map
                 bytype: 'layer',
                 limit: 50,
                 sort: this.$el.find('#sortBy').val()
@@ -133,16 +194,16 @@
             url: this.searchUrl,
             data: queryParameters
         }).done(function (data) {
-            _.each(data.rows, function (layer) {
+            $.each(data.rows, function (idx, layer) {
                 self.renderLayer(layer, showMeta);
             });
         });
 
     };
     LayerSearch.prototype.render = function () {
-        var doSearch = _.bind(this.doSearch, this);
+        var doSearch = Ext.createDelegate(this.doSearch, this);
 
-        this.$el.append(this.template());
+        this.$el.append(this.template.apply());
         this.$layerList = this.$el.find('#ms-search-layers ul');
 
         // populate the widget when its rendered
@@ -151,9 +212,11 @@
         // after the elements are added to the dom element attach the
         // events
 
-        this.$el.find('#done').click(_.bind(function () {
-            this.$el.remove();
-        }, this));
+        this.$el.find('#done').click(
+            Ext.createDelegate(function () {
+                this.$el.remove();
+            }, this)
+        );
 
         this.$el.find('#search').click(doSearch);
         this.$el.find('#query').blur(doSearch);
