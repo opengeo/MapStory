bab40c1cdd79c1ffc69dcb2d0b13c44dfe842c13
Ivan Willig <iwillig@gmail.com>
2013-04-29 21:08:59 -0400

    Add a first pass to the pagination for the search results in the add layer widget

--- media/script/map-search.js
+++ media/script/map-search.js
@@ -11,7 +11,7 @@
         '<p>',
         '<a class="show-meta" href="#">{title}</a> by ',
         '<a href="<%= layer.owner_detail %>">{owner}</a> on ',
-        '{last_modified}',
+        '{last_modified} ',
         '<a class="ms-add-to-map" href="#">Add to map</a>',
         '</p>',
         '</div>',
@@ -29,7 +29,7 @@
     '<div id="ms-header">',
       '<form>',
         '<fieldset>',
-          '<button id="search" type="submit" class="btn">Search</button>',
+          '<button id="search" type="submit">Search</button>',
           '<input id="query" type="text" class="search-query">',
           '<select id="sortBy">',
             '<option value="newest">Newest</option>',
@@ -45,6 +45,8 @@
           '<input id="bbox-limit" type="checkbox">',
           '<label>Show meta info expanded</label>',
           '<input id="show-meta-info" type="checkbox" checked>',
+        '<button id="prev">Prev</button>',
+        '<button id="next">Next</button>',
         '</fieldset>',
       '</form>',
     '</div>',
@@ -53,7 +55,7 @@
       '</ul>',
     '</div>',
     '<div id="ms-footer">',
-      '<button id="done" class="btn">Done</button>',
+      '<button id="done">Done</button>',
     '</div>').compile();
 
     LayerResult = function (options) {
@@ -142,8 +144,13 @@
     // main view object controls rendering widget template and
     // controls the events that are attached to this widget
     LayerSearch = function (options) {
+
         this.searchUrl = options.searchUrl;
         this.geoExplorer = options.geoExplorer;
+
+        this.pageSize = 2;
+        this.currentPage = 1;
+        this.numberOfRecords = 0;
 
         this.$el = $('<div/>', {
             id: 'ms-search-widget'
@@ -157,29 +164,78 @@
         this.template = widgetTemplate;
 
     };
+    LayerSearch.prototype.setPageButtons = function () {
+        this.setPrevButton();
+        this.setNextButton();
+    };
+
+    LayerSearch.prototype.setPrevButton = function () {
+        if (this.currentPage < 2) {
+            this.$el.find('#prev').attr('disabled', '');
+        } else {
+            this.$el.find('#prev').removeAttr('disabled');
+        }
+
+    };
+    LayerSearch.prototype.setNextButton = function () {
+        var button = this.$el.find('#next'),
+            currentLoc = this.currentPage * this.pageSize;
+
+        if (currentLoc >= this.numberOfRecords && this.numberOfRecords !== 0) {
+            this.$el.find('#next').attr('disabled', '');
+        } else {
+            this.$el.find('#next').removeAttr('disabled');
+        }
+    };
+
+    LayerSearch.prototype.getStart = function () {
+        return this.pageSize * this.currentPage - this.pageSize;
+    };
 
     LayerSearch.prototype.setLeft = function () {
         var widgetWidth = 600;
         this.$el.css('left', $(window).width() / 2 - widgetWidth / 2);
     };
 
-    LayerSearch.prototype.renderLayer = function (layer, showMeta) {
+    LayerSearch.prototype.renderLayer = function (layer) {
         var element = new LayerResult({
                 layer: layer,
                 geoExplorer: this.geoExplorer
-            }).render(showMeta);
+            }).render(this.showMeta);
 
         this.$layerList.append(element.$el);
     };
 
+    LayerSearch.prototype.renderLayers = function (layers) {
+        var self = this;
+        this.numberOfRecords = layers.total;
+
+         // if the start location is higher then the number of
+        // returned records, then reset the page number and redo the
+        // query
+        if (layers.total <= this.getStart() && layers.total !== 0) {
+            this.currentPage = 1;
+            this.doSearch();
+        }
+        this.$layerList.empty();
+        this.setPageButtons();
+        $.each(layers.rows, function (idx, layer) {
+            self.renderLayer(layer);
+        });
+
+    };
+
     LayerSearch.prototype.doSearch = function () {
-        var self = this,
-            showMeta = this.$el.find('#show-meta-info:checkbox').is(':checked'),
-            queryParameters = {
+        this.showMeta = this.$el.find(
+            '#show-meta-info:checkbox'
+        ).is(':checked');
+
+        var queryParameters = {
                 // hard code the type as it does not sense to add a
                 // map to another map
                 bytype: 'layer',
-                limit: 50,
+                limit: this.pageSize,
+                start: this.getStart(),
                 sort: this.$el.find('#sortBy').val()
             },
             q  = this.$el.find('#query').val();
@@ -188,18 +244,15 @@
             queryParameters.q = q;
         }
 
-        this.$layerList.empty();
-
         $.ajax({
             url: this.searchUrl,
             data: queryParameters
-        }).done(function (data) {
-            $.each(data.rows, function (idx, layer) {
-                self.renderLayer(layer, showMeta);
-            });
-        });
+        }).done(Ext.createDelegate(this.renderLayers, this));
+
 
     };
+
+
     LayerSearch.prototype.render = function () {
         var doSearch = Ext.createDelegate(this.doSearch, this);
 
@@ -211,7 +264,6 @@
 
         // after the elements are added to the dom element attach the
         // events
-
         this.$el.find('#done').click(
             Ext.createDelegate(function () {
                 this.$el.remove();
@@ -219,11 +271,34 @@
         );
 
         this.$el.find('#search').click(doSearch);
-        this.$el.find('#query').blur(doSearch);
         this.$el.find('#sortBy').change(doSearch);
         this.$el.find('#bbox-limit').change(doSearch);
         this.$el.find('#show-meta-info').change(doSearch);
+        this.$el.find('form').keypress(function (evt) {
+            // prevent the default event on the return key and redo
+            // the query
+            if (evt.which === 13) {
+                evt.preventDefault();
+                doSearch();
+            }
 
+        });
+        // TODO, these seem very similar see how to combine them
+        this.$el.find('#prev').click(Ext.createDelegate(function (event) {
+            event.preventDefault();
+            this.currentPage = this.currentPage - 1;
+            this.setPageButtons();
+            this.doSearch();
+        }, this));
+
+        this.$el.find('#next').click(Ext.createDelegate(function (event) {
+            event.preventDefault();
+            this.currentPage = this.currentPage + 1;
+            this.setPageButtons();
+            this.doSearch();
+        }, this));
+
+        this.setPageButtons();
         $('body').append(this.$el);
         return this;
     };
