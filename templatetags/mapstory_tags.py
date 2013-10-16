c9a222dee95af82a9fa2744cbb7d9a65b4abc27c
Ian Schneider <planablediglet@gmail.com>
2013-10-16 17:07:16 -0600

    entry points to messaging, refs #738

--- templatetags/mapstory_tags.py
+++ templatetags/mapstory_tags.py
@@ -503,6 +503,11 @@ def wiki_help_link(name, text='', title='Learn More', classes=''):
 def flag_widget(context, flag_object):
     return { 'flag_object': flag_object, 'request': context['request'] }
 
+
+@register.inclusion_tag('mapstory/_widget_message.html')
+def message_widget(user):
+    return { 'user' : user }
+
     
 @register.simple_tag
 def wiki_help_link(name, text='', title='Learn More', classes='', rel = ''):
